a1a4cf1f8f58092712103438e3b1862a21bd6206
ArmJav <91393155+ArmJav@users.noreply.github.com>
2024-10-26 00:19:46 +0300

    search

---
+++ src/widget/Search/ui/Search.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { Calendar } from '@/widget/Calendar';
+import { ref } from 'vue';
+
+const date_flight = ref()
+</script>
+
+<template>
+    <div class="flex w-full mx-[300px] gap-3 h-[50px]">
+        <input type="search" class="rounded h-full p-2" placeholder="Откуда">
+        <input type="search" class="rounded h-full p-2" placeholder="Куда">
+        <Calendar v-model="date_flight"/>
+        <button class="rounded bg-yellow w-[100px] hover:opacity-50 active:opacity-75">Найти</button>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
