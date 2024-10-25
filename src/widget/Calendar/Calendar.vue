f64419a41dd5c0e1e81619ac9741b1e7b6ee4a20
ArmJav <91393155+ArmJav@users.noreply.github.com>
2024-10-26 01:25:02 +0300

    caledar

--- src/widget/Calendar/Calendar.vue
+++ src/widget/Calendar/Calendar.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import locale from 'ant-design-vue/es/date-picker/locale/ru_Ru';
+import locale from 'ant-design-vue/es/date-picker/locale/en_US';
 import { Dayjs } from 'dayjs';
 import dayjs from "dayjs";
-import "dayjs/locale/ru";
-import ruRu from "ant-design-vue/es/locale/ru_Ru";
+import "dayjs/locale/en";
+import ruRu from "ant-design-vue/es/locale/en_US";
 
 dayjs.locale("ru");
 const model = defineModel<Dayjs>({required: true})
---
+++ src/widget/Header/ui/Header.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import logo from "@/assets/img/logo-header.svg";
+import { UserOutlined } from '@ant-design/icons-vue';
+import { router } from '@/router'
+</script>
+
+<template>
+    <div class="h-[100px] w-full flex justify-between bg-yellow px-8 py-5 items-center">
+        <img :src="logo" alt="logo" class="h-full">
+        <user-outlined class="text-[30px] text-white" @click="router.push('/')"/>
+    </div>
+</template>
