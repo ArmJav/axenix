27b4995dec574724d677c63aee935bb7fd757a31
ArmJav <91393155+ArmJav@users.noreply.github.com>
2024-10-26 05:38:45 +0300

    main page

--- src/widget/Header/ui/Header.vue
+++ src/widget/Header/ui/Header.vue
@@ -7,6 +7,9 @@ import { router } from '@/router'
 <template>
     <div class="h-[100px] w-full flex justify-between bg-yellow px-8 py-5 items-center">
         <img :src="logo" alt="logo" class="h-full">
-        <user-outlined class="text-[30px] text-white" @click="router.push('/')"/>
+        <div class="flex gap-6">
+            <a-button ghost>Найти</a-button>
+            <user-outlined class="text-[30px] text-white" @click="router.push('/')"/>
+        </div>
     </div>
 </template>
